3ceda06bc665b82840d953f79114ef2ebf8827ea
Guilherme Silva <guilhermejosesilva@gmail.com>
2019-05-29 09:33:36 +0100

    Fixed compile script

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-javac -cp src -d out/production/FEUP-SDIS-Proj2 src/com/network/Main.java src/
+javac -cp src -d out/production/FEUP-SDIS-Proj2 src/com/network/Main.java src/com/app/TestApp.java
