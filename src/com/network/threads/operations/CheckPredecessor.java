7c4a84917fbc311e45ce28c9965ce0ec8350c7df
Guilherme Silva <guilhermejosesilva@gmail.com>
2019-05-29 09:51:59 +0100

    Removed debug prints

--- src/com/network/threads/operations/CheckPredecessor.java
+++ src/com/network/threads/operations/CheckPredecessor.java
@@ -3,9 +3,7 @@ package com.network.threads.operations;
 import com.network.ChordNode;
 import com.network.info.InfoInterface;
 import com.network.info.NodeInfo;
-import com.network.log.NetworkLogger;
 
-import java.util.logging.Level;
 
 public class CheckPredecessor implements Runnable {
     private ChordNode node;
@@ -16,8 +14,6 @@ public class CheckPredecessor implements Runnable {
 
     @Override
     public void run() {
-        NetworkLogger.printLog(Level.INFO, "" + node.getSuccessor());
-        NetworkLogger.printLog(Level.INFO, "" + node.getPredecessor());
         final InfoInterface predecessor = this.node.getPredecessor();
         if(predecessor instanceof NodeInfo) {
             if (((NodeInfo) predecessor).getListener().getInternal().isClosed()) {
