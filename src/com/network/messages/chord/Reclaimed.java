154dc7616dc8a558c420f1ea53cca22baf987a43
Guilherme Silva <guilhermejosesilva@gmail.com>
2019-05-30 16:05:32 +0100

    Avoiding eternal reclaimed messages

--- src/com/network/messages/chord/Reclaimed.java
+++ src/com/network/messages/chord/Reclaimed.java
@@ -4,17 +4,27 @@ import com.network.ChordNode;
 import com.network.connections.listeners.MessageVisitor;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
 
 public class Reclaimed extends ChordMessage{
     private byte[] file_data;
     private BigInteger id;
     private String name;
+    private ArrayList<BigInteger> visited = new ArrayList<>();
 
     public Reclaimed(ChordNode node, byte[] file_data, BigInteger id, String name) {
         super(node);
         this.file_data = file_data;
         this.id = id;
         this.name = name;
+    }
+
+    public void visited(BigInteger v) {
+        visited.add(v);
+    }
+
+    public boolean alreadyVisited(BigInteger id) {
+        return this.visited.contains(id);
     }
 
     @Override
